0e0041fd6da889a347e2ef8a6a04ee888f8e84c2
Vladislav Telichko <vladislavteli@yandex-team.ru>
2019-06-26 15:49:33 +0500

    Remove TLDS

--- src/decorators/defaultMatchDecorator.js
+++ src/decorators/defaultMatchDecorator.js
@@ -1,10 +1,8 @@
 // @flow
 
 import LinkifyIt from 'linkify-it';
-import tlds from 'tlds';
 
 const linkify = new LinkifyIt();
-linkify.tlds(tlds);
 
 export default (text: string): Array<Object> => {
   return linkify.match(text);
